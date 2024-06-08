6947a97f5ce568365d14e51ce48076444c877d56
yahodska <29409202-yahodska@users.noreply.replit.com>
2024-06-08 08:53:38 +0000

    write to file volatile temperature

--- main.py
+++ main.py
@@ -13,17 +13,27 @@ def get_driver():
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
   driver = webdriver.Chrome(options=options)
-  driver.get("https://automated.pythonanywhere.com/login/")
+  driver.get("https://automated.pythonanywhere.com/")
   return driver
+
+def clean_text(text):
+  """Extract only the temperature from text"""
+  output = float(text.split(": ")[1])
+  return output
+
+def write_file(text):
+  """Write input text into a text file"""
+  filename = f"{time.strftime('%Y-%m-%d.%H-%M-%S')}.txt"
+  with open(filename, 'w') as file:
+    file.write(text)
   
 def main():
   driver = get_driver()
-  driver.find_element(by="id",value="id_username").send_keys("automated")
-  time.sleep(2)
-  driver.find_element(by="id", value="id_password").send_keys(
-    "automatedautomated" + Keys.RETURN)
-  print(driver.current_url)
-
+  while True:
+    time.sleep(2)
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    text = str(clean_text(element.text))
+    write_file(text)
 
 print(main())
 
